aa0523cce022e006529177791001ed491a2c2630
aniket umbc <aniket.umbc@gmail.com>
2018-01-31 08:08:25 +0530

    Add Model for search

---
+++ src/app/simple-http/search-result.model.ts
@@ -0,0 +1,17 @@
+/**
+ That holds individual records from youtube search
+ */
+export class SearchResult{
+    id:string;
+    title:string;
+    descriptions:string;
+    thumbUrl:string;
+    videoUrl:string;
+    constructor(obj?:any){
+        this.id=obj && obj.id || null;
+        this.title=obj && obj.title || null;
+        this.descriptions=obj && obj.descriptions|| null;
+        this.thumbUrl=obj && obj.thumbUrl || null;
+        this. videoUrl=obj && obj.videoUrl || `https://www.youtube.com/watch?v=${this.id}`;
+    }
+}
